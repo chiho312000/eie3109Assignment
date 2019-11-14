4fc8820b1baf74acc05dc935df099e5fefad8cc8
chiho312000 <36149077+chiho312000@users.noreply.github.com>
2019-11-14 11:07:42 +0800

    Branch code of part3 task 2

--- app/src/main/java/com/example/eie3109assignment/models/Panel.java
+++ app/src/main/java/com/example/eie3109assignment/models/Panel.java
@@ -103,10 +103,6 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback
                 graphic.getCoordinates().setX(x - bmpW);
                 graphic.getCoordinates().setY(y - bmpH);
 
-
-                int testTop, testLeft, testRight, testBottom;
-                int rectRight = x + bmpW * 2, rectBottom = y + bmpH * 2;
-
                 graphic.setAvailableSpace(new Rect(getLeft(), getTop(), getRight(), getBottom()));
                 for (GraphicObject graphic2 : graphics)
                 {
@@ -116,20 +112,6 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback
                         graphics.remove(graphic2);
                         return true;
                     }
-
-                    testTop = graphic2.getCoordinates().getY();
-                    testLeft = graphic2.getCoordinates().getX();
-                    testRight = testLeft + graphic2.getGraphic().getWidth();
-                    testBottom = testTop + graphic2.getGraphic().getHeight();
-
-                    if ((y >= testTop && y <= testBottom && x >= testLeft && x <= testRight)
-                        || (y >= testTop && y <= testBottom && rectRight >= testLeft && rectRight <= testRight)
-                        || (rectBottom >= testTop && rectBottom <= testBottom && x >= testLeft && x <= testRight)
-                        || (rectBottom >= testTop && rectBottom <= testBottom && rectRight >= testLeft && rectRight <= testRight))
-                    {
-                        return true;
-                    }
-
                 }
                 graphics.add(graphic);
             }
@@ -160,122 +142,11 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback
 
     public void updateMovement()
     {
-        updateSpaces();
         for (GraphicObject graphic : graphics)
         {
             graphic.move();
         }
 
-    }
-
-    public void updateSpaces()
-    {
-        GraphicObject graphicObject;
-        GraphicObject graphicObject1;
-
-        boolean hasMore = graphics.size() > 1;
-        Rect rect;
-
-        int graphicHeight, graphicWidth;
-        Coordinates originalCoordinates;
-        Coordinates coordinate1;
-        int top1, left1, right1, bottom1;
-        for (int i = 0 ; i < graphics.size(); i++)
-        {
-            graphicObject = graphics.get(i);
-            rect = new Rect(getLeft(), getTop(), getRight(), getBottom());
-            originalCoordinates = graphicObject.getCoordinates();
-            graphicHeight = graphicObject.getGraphic().getHeight();
-            graphicWidth = graphicObject.getGraphic().getWidth();
-            double movingAngle = Math.atan2((double)graphicObject.getMovement().getYSpeed() * graphicObject.getMovement().getYDirection(), (double)graphicObject.getMovement().getXSpeed() * graphicObject.getMovement().getXDirection()) + Math.PI;
-            movingAngle *= 180 / Math.PI;
-            int quadrant = (int)(movingAngle / 90) % 4;
-            Log.v("Panel", "moving direction: " + movingAngle + " quadrant :" + quadrant);
-            if (hasMore)
-            {
-                for (int j = 0; j < graphics.size(); j++)
-                {
-                    if (i == j) continue;
-                    graphicObject1 = graphics.get(j);
-                    coordinate1 = graphicObject1.getCoordinates();
-
-                    top1 = graphicObject1.getCoordinates().getY();
-                    left1 = graphicObject1.getCoordinates().getY();
-                    right1 = graphicObject1.getCoordinates().getX() + graphicObject1.getGraphic().getWidth();
-                    bottom1 = graphicObject1.getCoordinates().getY() + graphicObject1.getGraphic().getHeight();
-
-
-                    if (top1 < 0) top1 = 0;
-                    if (left1 < 0) left1 = 0;
-                    if (right1 > getWidth()) right1 = getWidth();
-                    if (bottom1 > getHeight()) bottom1 = getHeight();
-
-                    double angleToObject = Math.atan2((double)originalCoordinates.getY() - coordinate1.getY(), (double)(originalCoordinates.getX() - coordinate1.getX()));
-                    angleToObject *= 180 / Math.PI;
-
-                    Log.v("Panel", "direction towards object: " + angleToObject);
-
-                    if (Math.abs(movingAngle - angleToObject) < 30)
-                    {
-                        switch (quadrant)
-                        {
-                            case 0:
-                                if (originalCoordinates.getX() > right1) // update left
-                                {
-                                    if (right1 < rect.left && right1 > 0) rect.left = right1;
-                                    Log.v("Panel", "Update Left");
-                                }
-                                if (originalCoordinates.getY() > bottom1) // update top
-                                {
-                                    if (bottom1 > rect.top && bottom1 > 0) rect.top = bottom1;
-                                    Log.v("Panel", "Update top");
-                                }
-                                break;
-                            case 1:
-                                if (originalCoordinates.getX() + graphicWidth < left1) // update right
-                                {
-                                    if (left1 < rect.right && left1 > 0) rect.right = left1;
-                                    Log.v("Panel", "Update Right");
-                                }
-                                if (originalCoordinates.getY() > bottom1) // update top
-                                {
-                                    if (bottom1 > rect.top && bottom1 > 0) rect.top = bottom1;
-                                    Log.v("Panel", "Update Top");
-                                }
-                                break;
-                            case 2:
-                                if (originalCoordinates.getX() + graphicWidth < left1) // update right
-                                {
-                                    if (left1 < rect.right && left1 > 0) rect.right = left1;
-                                    Log.v("Panel", "Update Right");
-                                }
-                                if (originalCoordinates.getY() + graphicHeight < top1) // update bottom
-                                {
-                                    if (top1 < rect.bottom && top1 > 0) rect.bottom = top1;
-                                    Log.v("Panel", "Update Bottom");
-                                }
-                                break;
-                            case 3:
-                                if (originalCoordinates.getX() > right1) // update left
-                                {
-                                    if (right1 < rect.left && right1 > 0) rect.left = right1;
-                                    Log.v("Panel", "Update Left");
-                                }
-                                if (originalCoordinates.getY() + graphicHeight < top1) // update bottom
-                                {
-                                    if (top1 < rect.bottom && top1 > 0) rect.bottom = top1;
-                                    Log.v("Panel", "Update Bottom");
-                                }
-                                break;
-                        }
-                    }
-                }
-            }
-            graphicObject.setAvailableSpace(rect);
-            Log.v("Panel", String.format("available space of object %d: top: %d, left: %d , right: %d, bottom: %d", i, rect.top, rect.left, rect.right, rect.bottom));
-        }
-
-        Log.v("Panel", "update ends");
     }
 
     @NonNull
